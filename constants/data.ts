f83069d21fd18cf008ab8c9f0415d9cd77f8726b
Palabay Ronhiel C. <20palabayronhiel04@gmail.com>
2025-04-14 13:02:57 +0800

    Update data.ts

--- constants/data.ts
+++ constants/data.ts
@@ -1,59 +1,4 @@
 import icons from "./icons";
-import images from "./images";
-
-export const cards = [
-  {
-    title: "Card 1",
-    location: "Location 1",
-    price: "$100",
-    rating: 4.8,
-    category: "house",
-    image: images.newYork,
-  },
-  {
-    title: "Card 2",
-    location: "Location 2",
-    price: "$200",
-    rating: 3,
-    category: "house",
-    image: images.japan,
-  },
-  {
-    title: "Card 3",
-    location: "Location 3",
-    price: "$300",
-    rating: 2,
-    category: "flat",
-    image: images.newYork,
-  },
-  {
-    title: "Card 4",
-    location: "Location 4",
-    price: "$400",
-    rating: 5,
-    category: "villa",
-    image: images.japan,
-  },
-];
-
-export const featuredCards = [
-  {
-    title: "Featured 1",
-    location: "Location 1",
-    price: "$100",
-    rating: 4.8,
-    image: images.newYork,
-    category: "house",
-  },
-  {
-    title: "Featured 2",
-    location: "Location 2",
-    price: "$200",
-    rating: 3,
-    image: images.japan,
-    category: "flat",
-  },
-];
 
 export const categories = [
   { title: "All", category: "All" },
@@ -108,10 +53,6 @@ export const facilities = [
     icon: icons.laundry,
   },
   {
-    title: "Car Parking",
-    icon: icons.carPark,
-  },
-  {
     title: "Sports Center",
     icon: icons.run,
   },
@@ -124,42 +65,47 @@ export const facilities = [
     icon: icons.dumbell,
   },
   {
-    title: "Swimming pool",
-    icon: icons.swim,
+    title: "Pool",
+    icon: icons.pool,
+  },
+   {
+    title: "Spa",
+    icon: icons.spa,
+  },
+   {
+    title: "Sauna",
+    icon: icons.sauna,
+  },
+   {
+    title: "Arcade",
+    icon: icons.arcade,
+  },
+   {
+    title: "Elevator",
+    icon: icons.elevator,
+  },
+   {
+    title: "Bar",
+    icon: icons.bar,
+  },
+   {
+    title: "Park",
+    icon: icons.park,
+  },
+   {
+    title: "Fridge",
+    icon: icons.fridge,
+  },
+   {
+    title: "TV",
+    icon: icons.tv,
   },
   {
     title: "Wifi",
     icon: icons.wifi,
   },
   {
-    title: "Pet Center",
-    icon: icons.dog,
-  },
-];
-
-export const gallery = [
-  {
-    id: 1,
-    image: images.newYork,
-  },
-  {
-    id: 2,
-    image: images.japan,
-  },
-  {
-    id: 3,
-    image: images.newYork,
-  },
-  {
-    id: 4,
-    image: images.japan,
-  },
-  {
-    id: 5,
-    image: images.newYork,
-  },
-  {
-    id: 6,
-    image: images.japan,
+    title: "Air conditioning",
+    icon: icons.aircon,
   },
 ];
